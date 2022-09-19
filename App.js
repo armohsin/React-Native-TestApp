28768c88c07a98873c8323631ddb8e24259c79b6
armohsin <89929080+armohsin@users.noreply.github.com>
2022-09-19 14:29:03 +0500

    Basic-Commits

--- App.js
+++ App.js
@@ -1,16 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Linking, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
+  const titleText="Welcome to CybersolX";
+  const bodyText = "Please wait...";
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+        <Image
+          style={{ width: 250, height: 250 }}
+          resizeMode="contain"
+          source={require('./assets/yy3.gif')}
+        />
+        {/* LOWER TEXT */}
+        <Text style={styles.Heading}>
+          Welcome to CybersolX
+        </Text>
+        <Text style={styles.subtext} >{bodyText}
+        {"\n"}
+        or{"\n"} click the Button below to Visit us{"\n"}
+        </Text>
+
+        <Button
+          style={styles.button}
+          title="Visit Us"
+          onPress={()=>Linking.openURL('http://www.cybersolx.com')}    
+        />
+
       <StatusBar style="auto" />
-    </View>
+      </View>
+      
+    
   );
 }
 
+
 const styles = StyleSheet.create({
+  button:{
+    padding:20
+  },
+  subtext:{
+    textAlign:'center',
+    paddingBottom:10
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -18,3 +50,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
